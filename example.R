eb9c764c52a9ca64cc55d18257802a038f93ac98
Luke Smith <lukedansmi@sbcglobal.net>
2016-04-10 21:02:33 -0500

    Condensing functions into single .R files; created csv example

--- example.R
+++ example.R
@@ -1,3 +1,7 @@
+
+# yahoo.functions1.R EXAMPLE ----------------------------------------------
+
+
 sp500    <- read.csv("sp500.csv")
 set.seed(2016)
 sub      <- sample(1:504, 5)
@@ -6,19 +10,24 @@ test_run <- yahoo_scrape(stocks)
 preview  <- lapply(test_run, head, n = 3L)
 preview
 
-#find and extract dividends
-divs <- grep("Dividend", somestock$Open)
-somestock.divs      <- somestock[divs,]
-somestock.divs$Open <- gsub("\\s[A-Za-z]+", "", somestock.divs$Open)
+### find and extract dividends
+    dividends <- grep("Dividend", somestock$Open)
+    somestock.dividends      <- somestock[dividends,]
+    somestock.dividends$Open <- gsub("\\s[A-Za-z]+", "", somestock.dividends$Open)
 
-#find and extract splits
-splits <- grep("Split", somestock$Open)
-somestock.splits      <- somestock[splits,]
-somestock.splits$Open <- gsub("\n\\s+", "", somestock.splits$Open)
-somestock.splits$Open <- gsub("\\s[A-Za-z]", "", somestock.splits$Open)
+### find and extract splits
+    splits <- grep("Split", somestock$Open)
+    somestock.splits      <- somestock[splits,]
+    somestock.splits$Open <- gsub("\n\\s+", "", somestock.splits$Open)
+    somestock.splits$Open <- gsub("\\s[A-Za-z]", "", somestock.splits$Open)
 
-#extract everything that is not price
-not_price       <- sort(unique(c(divs, splits)))
-somestock.price <- somestock[-not_price,]
+### extract everything that is not price
+    not_price       <- sort(unique(c(dividends, splits)))
+    somestock.price <- somestock[-not_price,]
+    
+    this.stock <- list(somestock.price, somestock.dividends, somestock.splits)
 
-this.stock <- list(somestock.price, somestock.divs, somestock.splits)
+
+# yahoo.functions2.R EXAMPLE ----------------------------------------------
+
+
---
+++ yahoo.functions1.R
@@ -0,0 +1,70 @@
+
+# yahoo.recurse(ticker) ---------------------------------------------------
+
+yahoo.recurse <- function(ticker){
+    
+    ##build the url and read it
+    url   <- "https://finance.yahoo.com"
+    start <- "/q/hp?s="
+    end   <- "+Historical+Prices"
+    link  <- paste(url, start, ticker, end, sep = "")
+    yfp   <- read_html(link)
+    
+    ##set variables for the loop
+    i   <- 1
+    j   <- 1
+    yft <- list()
+    
+    while(i == 1){
+        i <- 0
+        
+        ##read the table
+        y.table            <- yfp %>%
+            html_nodes("table.yfnc_datamodoutline1") %>%
+            html_nodes("table") %>%
+            html_table()
+        y.table            <- y.table[[1]]
+        y.table            <- y.table[-(dim(y.table)[1]), ]
+        yft[[ticker]][[j]] <- y.table
+        
+        ##search for the "Next" button
+        n    <- html_nodes(yfp, "a")
+        rel  <- html_attr(n, "rel")
+        link <- n[grep("next", rel)]
+        
+        if (!length(link) == 2){
+            yfp           <- ""
+            yft[[ticker]] <- yft[[ticker]] %>%
+                ldply()
+            yft[[ticker]]
+            break
+        }
+        
+        ##set the link if "Next" button exists
+        next_link <- link %>%
+            html_attr("href") %>%
+            unique()
+        yfp       <- paste(url, next_link, sep = "") %>%
+            read_html()
+        
+        ##prepare for the next round
+        i <- i + 1
+        j <- j + 1
+        Sys.sleep(3)
+    }
+    yft[[ticker]]
+}
+
+
+# yahoo.scrape(ticker) ----------------------------------------------------
+
+yahoo.scrape <- function(ticker){
+    stopifnot(is.character(ticker))
+    source("yahoo.recurse.R")
+    source("check.pkgs.R")
+    if (!check.pkgs("rvest")) library(rvest)
+    if (!check.pkgs("plyr")) library(plyr)
+    yahoo.results        <- lapply(ticker, yahoo.recurse)
+    names(yahoo.results) <- ticker
+    yahoo.results
+}
